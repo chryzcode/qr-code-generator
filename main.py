efff3a9561b939531cfb97b4d0c916ca88e3014f
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-01-01 05:55:40 +0100

    commit

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import qrcode
+try:
+    data = input('Enter the data to be encoded: ')
+    if len(data) == 0:
+        data = 'https://www.youtube.com/channel/UCxro6LYOp3pmmuWDPMg-p1Q'
+except:
+    print('Error: Invalid input')
 
-data = 'https://www.youtube.com/channel/UCxro6LYOp3pmmuWDPMg-p1Q'
 qr = qrcode.QRCode( version = 1, box_size = 15, border = 5)
 
 qr.add_data(data)
 qr.make(fit = True)
 img = qr.make_image(fill_color = 'black', back_color = 'white')
 
-img.save('C:/Users/chryz/Desktop/fun/chryzhubyoutubegrcode.png')
+img.save('./qrcode/qrcode.png')
